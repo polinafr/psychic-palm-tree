f602becc13320652ff898347c9127328634c676d
polinafr <Zapadniki1836>
2021-08-08 21:29:12 +0300

    Good class changed

--- BELayer/Good.cs
+++ BELayer/Good.cs
@@ -7,7 +7,10 @@ namespace BELayer
     public class Good
     {
         enum GoodType { Nutrition, Clothes, Communication, Cleaning, Infants, Pets};
-        private QRCode Code;
+        private int Id;
+        private float price;
+        private String Shop;
+        private float Quantity;
         private string Picture { get; set; }
         private string Description { get; set; }
         private GoodType Type;
