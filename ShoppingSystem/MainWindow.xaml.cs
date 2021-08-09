902b3414dd4016149cc2f129f5f87b78357f8903
טליה ישראל <t0505154143@gmail.com>
2021-08-09 18:12:46 +0300

    new

--- ShoppingSystem/MainWindow.xaml.cs
+++ ShoppingSystem/MainWindow.xaml.cs
@@ -23,6 +23,51 @@ namespace ShoppingSystem
         public MainWindow()
         {
             InitializeComponent();
+            WindowState = WindowState.Maximized;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            AddGood newWindow = new AddGood();
+            newWindow.Closed += (s, args) => this.Show();
+
+            newWindow.Show();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            UpDateGood newWindow = new UpDateGood();
+            newWindow.Closed += (s, args) => this.Show();
+            newWindow.Show();
+                
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            NewShop newWindow = new NewShop();
+            newWindow.Closed += (s, args) => this.Show();
+            newWindow.Show();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            PreviousShop newWindow = new PreviousShop();
+            newWindow.Closed += (s, args) => this.Show();
+            newWindow.Show();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            StatisticsAndRecommendations newWindow = new StatisticsAndRecommendations();
+            newWindow.Closed += (s, args) => this.Show();
+            newWindow.Show();
         }
     }
 }
